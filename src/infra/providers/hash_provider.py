c7d025a7400252f63a65a6da8f6b46f4104dee75
thiagombrs <138166562+thiagombrs@users.noreply.github.com>
2025-04-24 14:24:44 -0300

    Outros restantes src > src

--- src/infra/providers/hash_provider.py
+++
@@ -1,9 +0,0 @@
-from passlib.context import CryptContext
-
-pwd_context = CryptContext(schemes=['bcrypt'])
-
-def gerar_hash(texto):
-    return pwd_context.hash(texto)
-
-def verificar_hash(texto, hash):
-    return pwd_context.verify(texto, hash)
